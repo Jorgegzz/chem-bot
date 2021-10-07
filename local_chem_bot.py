866cc1107fda403b2949d30cd17cb12a9fde361c
Jorgegzz <A01571510@tec.mx>
2021-10-07 07:10:33 -0500

    local_update.py

--- local_chem_bot.py
+++ local_chem_bot.py
@@ -6,10 +6,10 @@ from discord.ext import commands
 from collections import Counter
 
 
-TOKEN = "TOKEN"  # Change bot token
+TOKEN = getenv('TOKEN')
 PREFIX = "totno "
 client = commands.Bot(command_prefix = PREFIX, activity = discord.Game(name = PREFIX))
-
+client.remove_command("help")
 
 @client.event
 async def on_ready():
@@ -22,13 +22,41 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send(
             embed=discord.Embed(
-                description=f'**Command not found!.**',
+                description=f'**Command not found!**',
                 color=discord.Color.red()),
-            delete_after=5.0
+        )
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send(
+            embed=discord.Embed(
+                description='**Compound not found!**',
+                color=discord.Color.red()),
         )
 
 
-# Search #
+# Help Command #
+async def help(ctx):
+    embed = discord.Embed(
+        title = "List of commands",
+        color = discord.Color.green()
+    )
+    embed.add_field(
+        name="*chem search* (compound name or formula)",
+        value="Returns compound info",
+        inline=False
+    )
+    embed.add_field(
+        name="chem balance ( chemical equation Ex. *C + H3O = CO2 + H* )",
+        value="Returns balanced equation (Verifies individual"
+              " compounds but doesnt verify if the reaction itself is possible)\n",
+        inline=False
+    )
+    embed.set_footer(
+        text = "Warning: this bot is still being developed and you may encounter errors"
+    )
+    await ctx.send(ctx.message.author, embed=embed)
+
+
+# Search Equations #
 async def search_mf(name_name):
     name_results = pcp.get_compounds(name_name, "name")
     if bool(name_results):
@@ -46,18 +74,18 @@ async def search_name(name_mf):
 
     r_cid = sorted(keys)[0]
     result_compound = pcp.Compound.from_cid(r_cid)
-
     return result_compound
 
 
 @client.command()
 async def search(ctx, *, search_type_args):
-    scm = (" ".join(search_type_args.split()))
-    found_cpmd = await search_mf(scm)
+    async with ctx.typing():
+        scm = (" ".join(search_type_args.split()))
+        found_cpmd = await search_mf(scm)
 
-    if not found_cpmd:
-        smf = search_type_args.split()
-        found_cpmd = await search_name(smf)
+        if not found_cpmd:
+            smf = search_type_args.split()
+            found_cpmd = await search_name(smf)
 
     embed = discord.Embed(
         title=found_cpmd.synonyms[0],
@@ -121,58 +149,54 @@ async def search(ctx, *, search_type_args):
     await ctx.send(embed=embed)
 
 
+# Balance Equations #
+async def get_comp_link(reac_prod):
+    splt = reac_prod.split("+")
+    output_comp = ""
+    for ind, comp in enumerate(splt):
+        found_comp = await search_name(comp)
+        reac_link = f"[{comp}](https://pubchem.ncbi.nlm.nih.gov/compound/{found_comp.cid})"
+        if ind != 0:
+            output_comp += f"+ {reac_link}"
+        else:
+            output_comp += reac_link
+    return output_comp
+
+
 @client.command()
 async def balance(ctx, *, equation):
     split_equation = equation.split("=", 1)
     reactants = split_equation[0]
     products = split_equation[1]
 
-    await ctx.send("Loading (This may take a while)...")
-    embed = discord.Embed(
-        title = "Balance Equation",
-        color = discord.Color.blue()
-    )
+    async with ctx.typing():
+        embed = discord.Embed(
+            title = "Balance Equation",
+            color = discord.Color.blue()
+        )
 
-    embed.set_author(
-        name=f"Requested by {ctx.author.display_name}",
-        icon_url=ctx.author.avatar_url
-    )
-    splt_reactants = products.split("+")
-    output_reacs = ""
-    for ind, reac in enumerate(splt_reactants):
-        found_reac = await search_name(reac)
-        reac_link = f"[{reac}](https://pubchem.ncbi.nlm.nih.gov/compound/{found_reac.cid})"
-        if ind != 0:
-            output_reacs += f"+ {reac_link}"
-        else:
-            output_reacs += reac_link
-    embed.add_field(
-        name="**Reactants**",
-        value = output_reacs,
-        inline = True
-    )
+        embed.set_author(
+            name=f"Requested by {ctx.author.display_name}",
+            icon_url=ctx.author.avatar_url
+        )
 
-    splt_products = products.split("+")
-    output_prods = ""
-    for ind, prod in enumerate(splt_products):
-        found_prod = await search_name(prod)
-        prod_link = f"[{prod}](https://pubchem.ncbi.nlm.nih.gov/compound/{found_prod.cid})"
-        if ind != 0:
-            output_prods += f" + {prod_link}"
-        else:
-            output_prods += prod_link
+        embed.add_field(
+            name="**Reactants**",
+            value = await get_comp_link(reactants),
+            inline = True
+        )
 
-    embed.add_field(
-        name="**Products**",
-        value = output_prods,
-        inline = True
-    )
+        embed.add_field(
+            name="**Products**",
+            value = await get_comp_link(products),
+            inline = True
+        )
 
-    embed.add_field(
-        name="**Balanced**",
-        value = f"{eb.balance(reactants, products)}".replace("**1**", ""),
-        inline = False
-    )
+        embed.add_field(
+            name="**Balanced**",
+            value = f"{eb.balance(reactants, products)}".replace("**1**", ""),
+            inline = False
+        )
     await ctx.send(embed=embed)
 
 
